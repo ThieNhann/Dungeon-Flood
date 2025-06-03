a6cfba2b573abd9e73bf3ce338c3e396ed6744ac
Thiện Nhân <ngtnhan14@gmail.com>
2025-06-03 22:27:10 +0700

    adjust: chỉnh thứ tự layout tường cho đẹp

--- Wall.cpp
+++ Wall.cpp
@@ -45,14 +45,15 @@ void HorizontalWall::UnloadWallTexture() {
 
 void WallManager::InitCornerWalls() {
     walls.clear();
-    walls.emplace_back(new HorizontalWall({0, -15}));
-    walls.emplace_back(new VerticalWall({0, 0}));
+    
     walls.emplace_back(new VerticalWall({0, 600}));
-    walls.emplace_back(new HorizontalWall({820, -15}));
-    walls.emplace_back(new VerticalWall({1390, 0}));
     walls.emplace_back(new VerticalWall({1390, 600}));
+    walls.emplace_back(new HorizontalWall({0, -15}));
+    walls.emplace_back(new HorizontalWall({820, -15}));
     walls.emplace_back(new HorizontalWall({0, 850}));
     walls.emplace_back(new HorizontalWall({820, 850}));
+    walls.emplace_back(new VerticalWall({0, 0}));
+    walls.emplace_back(new VerticalWall({1390, 0}));
 }
 
 void WallManager::Draw() {
